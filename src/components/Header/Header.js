0a5df5d1f0770c255d6426b507d2632b6562a74d
matthias.paulitsch <matthias.paulitsch@e-mundo.de>
2023-02-24 11:46:05 +0100

    ex-2: implement solution for exercise-2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, WEIGHTS, QUERIES } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+
+import Icon from '../Icon';
+import UnstyledButton from '../UnstyledButton';
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -29,7 +32,18 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
-        <Side />
+        <MobileNav>
+          <UnstyledButton className="mobile">
+            <Icon id="shopping-bag" strokeWidth={2} />
+          </UnstyledButton>
+          <UnstyledButton className="mobile">
+            <Icon id="search" strokeWidth={2} />
+          </UnstyledButton>
+          <UnstyledButton className="mobile" onClick={() => setShowMobileMenu(true)}>
+            <Icon id="menu" strokeWidth={2} />
+          </UnstyledButton>
+        </MobileNav>
+        <Side className="hide-on-mobile"/>
       </MainHeader>
 
       <MobileMenu
@@ -46,16 +60,53 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media(${QUERIES.phone}) {
+    padding: 16px;
+  }
+
+  @media(${QUERIES.tablet}) {
+    border-top: solid 6px;
+    border-color: ${COLORS.gray[900]};
+    align-items: center;
+    justify-content: space-between;
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+
+  @media(${QUERIES.tablet}) {
+    display: none;
+  }
 `;
+
+const MobileNav = styled.div`
+  display: none;
+
+  @media(${QUERIES.tablet}) {
+    display: flex;
+    justify-content: space-between;
+    gap: 40px;
+  }
+
+  @media(${QUERIES.phone}) {
+    &:not(:last-of-type) {
+      gap: 24px;
+    }
+  }
+`
 
 const Side = styled.div`
   flex: 1;
+
+  @media(${QUERIES.tablet}) {
+    &.hide-on-mobile {
+      flex: 0;
+    }
+  }
 `;
 
 const NavLink = styled.a`
