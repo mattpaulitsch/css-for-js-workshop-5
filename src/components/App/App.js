4fdf7cd8be6302c2a9896f8ad0a5fade16faaa7d
matthias.paulitsch <matthias.paulitsch@e-mundo.de>
2023-02-24 09:51:51 +0100

    ex-1: implement solution to first exercise
    added some testing for media queries in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,8 @@ import styled from 'styled-components/macro';
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
 
+import { QUERIES } from '../../constants.js'
+
 const App = () => {
   const [sortId, setSortId] = React.useState('newest');
 
@@ -19,6 +21,19 @@ const App = () => {
 
 const Main = styled.main`
   padding: 64px 32px;
+  background: pink;
+
+  @media(${QUERIES.laptop}) {
+    background: red;
+  }
+
+  @media(${QUERIES.tablet}) {
+    background: green;
+  }
+
+  @media(${QUERIES.phone}) {
+    background: yellow;
+  }
 `;
 
 export default App;
